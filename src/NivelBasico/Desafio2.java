ca4dddb705e11143939803c4df38d1b71276607a
Lucas Vidal <lucasvidal.gs@gmail.com>
2025-03-12 20:57:09 -0300

    Adicionando remoção e arrumando bug de scanner

--- src/NivelBasico/Desafio2.java
+++ src/NivelBasico/Desafio2.java
@@ -14,9 +14,10 @@ public class Desafio2 {
         System.out.println();
 
         boolean rodaPrograma = true;
-        int NUMERO_MAXIMO = 5;
+        final int NUMERO_MAXIMO = 5;
 
         String[] ninjas = new String[NUMERO_MAXIMO];
+        int escolha = 0;
 
         int ninjasCadastrados = 0;
 
@@ -25,17 +26,23 @@ public class Desafio2 {
             System.out.println("1 - Cadastrar novo ninja");
             System.out.println("2 - Listar ninjas existentes");
             System.out.println("3 - Limpar lista existente");
-            System.out.println("4 - Sair");
+            System.out.println("4 - Deletar ninja");
+            System.out.println("5 - Sair");
 
-            int escolha = scanner.nextInt();
-
+            escolha = scanner.nextInt();
+        scanner.nextLine();
 
             switch (escolha) {
                 case 1:
                     if(ninjasCadastrados < NUMERO_MAXIMO) {
                         System.out.println("Digite o nome do ninja: ");
-                        String novoNinja = scanner.next();
-                        ninjas[ninjasCadastrados] = novoNinja;
+                        String novoNinja = scanner.nextLine();
+                        for (int i = 0; i < ninjas.length; i++) {
+                            if(ninjas[i] == null) {
+                                ninjas[i] = novoNinja;
+                                break;
+                            }
+                        }
                         ninjasCadastrados++;
                         System.out.println("----- NINJA CADASTRADO COM SUCESSO -----");
                     } else {
@@ -47,11 +54,12 @@ public class Desafio2 {
                     if(ninjasCadastrados == 0) {
                         System.out.println("----- NÃO EXISTEM NINJAS CADASTRADOS -----");
                     } else {
-                        System.out.println("----- NINJAS CADASTRADOS -----");
+                        System.out.println("=========== NINJAS CADASTRADOS ===========");
                         for(int i = 0; i < ninjas.length; i++) {
-                            String nomeDoNinja = ninjas[i] != null ? ninjas[i] : "Não cadastrado.";
-                            System.out.println(i+1 + " - " + nomeDoNinja);
+                            String nomeDoNinja = ninjas[i] != null ? ninjas[i] : "* Não cadastrado *";
+                            System.out.println(i+1 + " - " + nomeDoNinja.toUpperCase());
                         }
+                        System.out.println("=========================================");
                     }
 
                     break;
@@ -63,6 +71,19 @@ public class Desafio2 {
                     break;
 
                 case 4:
+                    System.out.println("----- DELETANDO NINJA -----");
+                    if(ninjasCadastrados > 0) {
+                        System.out.print("Informe o número do ninja que você quer deletar da lista:  ");
+                        int ninjaDeletado = scanner.nextInt();
+                        ninjas[ninjaDeletado - 1] = null;
+                        ninjasCadastrados--;
+                        System.out.printf("\nO ninja n°%d foi excluido com sucesso!\n", ninjaDeletado);
+                    } else {
+                        System.out.println("Não há ninjas para serem deletados.");
+                    }
+                    break;
+
+                case 5:
                     rodaPrograma = false;
                     System.out.println("Obrigado por usar nosso sistema.");
                     break;
@@ -73,7 +94,7 @@ public class Desafio2 {
         }
 
 
-
+        scanner.close();
 
     }
 
